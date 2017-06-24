1ea94af96b41b74e567a909072d42cd23f917340
Sébastien Pennec <sebastien@MBP17-Seb.local>
2017-06-24 16:09:10 +0200

    test

--- src/test/kotlin/ch/pennec/kotlin/business/CustomerSelectorTest.kt
+++ src/test/kotlin/ch/pennec/kotlin/business/CustomerSelectorTest.kt
@@ -12,8 +12,7 @@ class CustomerSelectorTest {
     @Test fun simple() {
         val c1 = Customer("name1", "name2");
         val c2 = Customer("name1name1", "name2");
-        val selector = CustomerSelector();
-        assertEquals(c2, selector.select(listOf(c1, c2)));
+        assertEquals(c2, CustomerSelector().select(listOf(c1, c2)));
     }
 
 
